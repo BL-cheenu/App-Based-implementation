9d87423b3c018d99efcb726ca6f9e4d72e81f14f
cheenum <38590526+cheenum@users.noreply.github.com>
2025-12-30 11:37:28 +0530

    OopsBannerAppUC1

---
+++ src/com/bl/app/based/implementation/OopsBannerAppUC1.java
@@ -0,0 +1,17 @@
+package com.bl.app.based.implementation;
+
+/**
+ * * OOPS Banner App UC1 – OOPS Banner Display Application
+ * <p>
+ * This class demonstrates a simple Java application
+ * that displays the Object Oriented Programming System (OOPS)
+ * acronym to the console.
+ *
+ * @author cheenu
+ * @version 1.0
+ */
+public class OopsBannerAppUC1 {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
--- src/com/bl/app/based/implementation/UC1.java
+++
@@ -1,7 +0,0 @@
-package com.bl.app.based.implementation;
-
-public class UC1 {
-    public static void main(String[] args) {
-
-    }
-}
