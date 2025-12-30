a8edfc224d4455782e65be2ee8f0bdf74bada748
cheenum <38590526+cheenum@users.noreply.github.com>
2025-12-30 12:05:20 +0530

    OopsBannerAppUC2

---
+++ src/com/bl/app/based/implementation/OopsBannerAppUC2.java
@@ -0,0 +1,32 @@
+package com.bl.app.based.implementation;
+
+/**
+ * OOPSBannerApp UC2 - OOPS Banner Display Application
+ * <p>
+ * This class demonstrates the creation of ASCII art banner using Object-Oriented Programming principles.
+ * It extends the basic banner concept by displaying the word "OOPS" in a visual format using asterisks (*)
+ * and spaces to form each letter.
+ *
+ * @author User
+ * @version 2.0
+ */
+
+public class OopsBannerAppUC2 {
+    public static void main(String[] args) {
+        String[] banner = {
+                "  ***       ***     ******      *****",
+                "**   **  **     **  **    **   **",
+                "**   **  **     **  **     ** **",
+                "**   **  **     **  **    **    **",
+                "**   **  **     **  *****         ***",
+                "**   **  **     **  **               **",
+                "**   **  **     **  **                **",
+                "**   **  **     **  **              **",
+                "  ***       ***     **          *****"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
